a8b78a0aa9cd2f5bf2aa40352bdffc6ee5f11760
Thomas Schenker <tommzn@gmx.de>
2021-12-12 15:53:35 +0100

    fix: add datasources

---
+++ datasources.go
@@ -0,0 +1,10 @@
+package core
+
+type DataSource string
+
+const (
+	DATASOURCE_BILLINGREPORT DataSource = "billingreport"
+	DATASOURCE_WEATHER       DataSource = "weather"
+	DATASOURCE_INDOORCLIMATE DataSource = "indoorclimate"
+	DATASOURCE_EXCHANGERATE  DataSource = "exchangerate"
+)
